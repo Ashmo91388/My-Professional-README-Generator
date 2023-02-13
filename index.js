84d4151024c0dd0616a01e37fce269e826cd31f5
cameronmcnz~clear <ashly_mossette@yahoo.com>
2023-02-13 11:29:31 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your projects title',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github user name?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your projecrt?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project?',
+        choices: ['APACHE 2.0', 'MIT', 'GPL 3.0', 'BSD 3', 'None']
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide instructions for installation',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Provide instructions for running the test',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions on how to use the app',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide instructions on how to contribute to this app',
+      },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
